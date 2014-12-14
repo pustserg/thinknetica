9a6366b17c318400d36fd43ba8997ba890a7c190
pustserg <psakaskif@gmail.com>
2014-12-14 18:33:01 +0300

    added sign_up feature test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  devise_for :users, :path => '', :path_names => {
+    :sign_up => 'signup',
+    :sign_in => 'login', 
+    :sign_out => 'logout'
+  }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'User sign up', %q{
+  In order to be able to ask question
+  As a guest
+  I want to be able to sign up
+} do
+
+  scenario 'Guest tries to sign up' do
+    visit '/signup'
+    fill_in 'Email', with: "useremail@think.test"
+    fill_in 'Password', with: "testpassword"
+    fill_in 'Password confirmation', with: "testpassword"
+    click_on 'Sign up'
+
+
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+    expect(current_path).to eq root_path
+  end
+  
+end
