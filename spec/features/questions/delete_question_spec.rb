ecd198cbe234a85f4b1780372bb31e214970b9ad
pustserg <psakaskif@gmail.com>
2014-12-15 16:56:43 +0400

    added feature spec for question deletition

---
+++ spec/features/questions/delete_question_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+feature 'delete question', %q{
+  In order to remove silly question
+  As an author of question
+  I want to be able my question if it has not answer
+} do
+
+  given(:jack) { create(:user) }
+  given(:dave) { create(:user) }
+  given(:question) { jack.questions.create(attributes_for(:question)) }
+
+  scenario 'user tries to delete his question' do
+
+    sign_in(jack)
+    visit question_path(question)
+
+    click_on "Delete question"
+
+    expect(page).to_not have_content question.title
+    expect(current_path).to eq questions_path
+  end
+
+  scenario 'user tries to remove question of another user' do
+
+    sign_in(dave)
+    visit question_path(question)
+
+    expect(page).to_not have_content 'Delete question'
+
+  end
+
+  scenario 'guest tries to remove question' do
+
+    visit question_path(question)
+
+    expect(page).to_not have_content 'Delete question'
+
+  end
+
+end
