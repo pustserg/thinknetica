ab42379b73fc19904a472b4027745a252d128d8b
pustserg <psakaskif@gmail.com>
2014-12-24 19:37:28 +0300

    user can attach file to question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -47,7 +47,7 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:title, :body)
+    params.require(:question).permit(:title, :body, attachments_attributes: [:file])
   end
 
   # def check_author
