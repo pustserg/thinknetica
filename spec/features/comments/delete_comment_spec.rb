5943959699687083c7022a67cc8c6a865e235f52
pustserg <psakaskif@gmail.com>
2014-12-24 10:56:05 +0300

    new feature spec for deletition comment

---
+++ spec/features/comments/delete_comment_spec.rb
@@ -0,0 +1,15 @@
+require 'feature_helper'
+
+feature 'Author can delete his comment', %q{
+  In order to delete silly comment
+  As an comment author 
+  I want to be able to delete comment
+} do 
+
+  scenario 'Guest tries to delete comment'
+
+  scenario 'User tries to delete another user comment'
+
+  scenario 'User tries to delete his comment'
+  
+end
