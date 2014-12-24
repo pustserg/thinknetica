f012efe22e1cc8b7866c11d1dfd26de3f7dfb88d
pustserg <psakaskif@gmail.com>
2014-12-24 11:35:27 +0300

    user can delete his comment

--- spec/features/comments/delete_comment_spec.rb
+++ spec/features/comments/delete_comment_spec.rb
@@ -6,10 +6,32 @@ feature 'Author can delete his comment', %q{
   I want to be able to delete comment
 } do 
 
-  scenario 'Guest tries to delete comment'
+  given(:dave) { create(:user) }
+  given(:jack) { create(:user) }
+  given(:question) { create(:question) }
+  given(:jack_comment) { create(:question_comment, user: jack, body: 'jack comment') }
+  
+  scenario 'Guest tries to delete comment' do
+    visit question_path(jack_comment.commentable)
+    # save_and_open_page
+    expect(page).to_not have_content 'Delete comment'
+  end
 
-  scenario 'User tries to delete another user comment'
+  scenario 'User tries to delete another user comment' do
+    sign_in dave
+    visit question_path(jack_comment.commentable)
+    
+    expect(page).to_not have_content 'Delete comment'
 
-  scenario 'User tries to delete his comment'
+  end
+
+  scenario 'User tries to delete his comment' do
+    sign_in jack
+    visit question_path(jack_comment.commentable)
+
+    click_on 'Delete comment'
+
+    expect(page).to_not have_content 'jack_comment'
+  end
   
 end
