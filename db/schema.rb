1b9840587512d560d358652528ef2732aeb7c841
pustserg <psakaskif@gmail.com>
2014-12-25 14:32:00 +0300

    fixed indexes at attachments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225084847) do
+ActiveRecord::Schema.define(version: 20141225111857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,7 @@ ActiveRecord::Schema.define(version: 20141225084847) do
     t.string   "attachmentable_type"
   end
 
-  add_index "attachments", ["attachmentable_id"], name: "index_attachments_on_attachmentable_id", using: :btree
-  add_index "attachments", ["attachmentable_type"], name: "index_attachments_on_attachmentable_type", using: :btree
+  add_index "attachments", ["attachmentable_id", "attachmentable_type"], name: "index_attachments_on_attachmentable_id_and_attachmentable_type", using: :btree
 
   create_table "comments", force: true do |t|
     t.text     "body",             null: false
