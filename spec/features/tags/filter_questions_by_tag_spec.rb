1160881cb59ef48b82030bdae7e1c9fd99824c95
pustserg <psakaskif@gmail.com>
2014-12-30 09:00:54 +0300

    added spec for filter question by tag

---
+++ spec/features/tags/filter_questions_by_tag_spec.rb
@@ -0,0 +1,16 @@
+require 'feature_helper'
+
+feature 'Guest can filter question list by tag' do
+
+  given(:user) { create(:user) }
+  given!(:first_question) { create(:question, user: user, title: 'first', tag_list: 'first') }
+  given!(:second_question) { create(:question, user: user, title: 'second', tag_list: 'second') }
+
+  scenario 'when visit question index page with tag' do
+    visit tag_path('first')
+    
+    expect(page).to have_content 'first'
+    expect(page).to_not have_content 'second'
+  end
+
+end
