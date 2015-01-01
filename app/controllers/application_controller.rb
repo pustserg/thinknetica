de4732c5c5e7c04ebf0dd8fecd1d42b2a7aac2b3
pustserg <psakaskif@gmail.com>
2015-01-01 23:42:39 +0300

    user can filter questions by tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_tag_cloud
+
 
   protected
 
@@ -10,5 +12,9 @@ class ApplicationController < ActionController::Base
     if current_user != @resource.user
       render json: { error: "fufufu" }, status: 403
     end
+  end
+
+  def set_tag_cloud
+    @tag_cloud = Tag.all
   end
 end
