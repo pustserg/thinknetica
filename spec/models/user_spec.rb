ae973abb39df26564a3be9c28bda0de7d5750f1d
pustserg <psakaskif@gmail.com>
2015-01-15 09:24:49 +0300

    added tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,19 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
+
+  let(:user) { create(:user) }
+  let(:another_user) { create(:user) }
+  let!(:question) { create(:question, user: user) }
+  let!(:answer) { create(:answer, user: user, question: question) }
+  let!(:comment) { create(:question_comment, commentable: question, user: user) }
+
+  before do
+    question.vote_up(another_user)
+    answer.vote_up(another_user)
+    comment.vote_down(another_user)
+  end
+
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
@@ -28,4 +41,28 @@ RSpec.describe User, :type => :model do
   it { should have_many :answers }
   it { should have_many :comments }
   it { should have_many :votes }
+
+  describe 'user_votes' do
+    it 'returns votes which user got for his questions/answers/comments' do
+      expect(user.user_votes).to match_array [question.votes.first, answer.votes.first, comment.votes.first]
+    end
+  end
+
+  describe 'likes' do
+    it 'returns likes which user got for his questions/answers/comments' do
+      expect(user.likes).to match_array [question.votes.first, answer.votes.first]
+    end
+  end
+
+  describe 'dislikes' do
+    it 'returns dislikes which user got for his questions/answers/comments' do
+      expect(user.dislikes).to match_array [comment.votes.first]
+    end
+  end
+
+  describe 'karma' do
+    it 'returns likes.count - dislikes.count' do
+      expect(user.karma).to eq 1
+    end
+  end
 end
