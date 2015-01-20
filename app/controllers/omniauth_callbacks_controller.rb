9213e1472c3d945fcc41e04192850d8c5c3d5fbb
pustserg <psakaskif@gmail.com>
2015-01-20 19:10:27 +0300

    changed user logic to resource

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,7 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
+  TEMP_EMAIL_REGEX = /.temp/
+
   def facebook
     # render json: request.env['omniauth.auth']
     @user = User.find_for_oauth(request.env['omniauth.auth'])
@@ -11,17 +13,32 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def twitter
     @user = User.find_for_oauth(request.env['omniauth.auth'])
-    redirect_to finish_signup_path(@user)
+    if @user.email =~ USER::TEMP_EMAIL_REGEX
+      redirect_to edit_user_path(@user)
+    else
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'twitter') if is_navigational_format?
+    end
   end
 
   def github
     @user = User.find_for_oauth(request.env['omniauth.auth'])
-    redirect_to finish_signup_path(@user)
+    if @user.email =~ USER::TEMP_EMAIL_REGEX
+      redirect_to edit_user_path(@user)
+    else
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'github') if is_navigational_format?
+    end
   end
 
   def vkontakte
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    redirect_to finish_signup_path(@user)
+        @user = User.find_for_oauth(request.env['omniauth.auth'])
+    if @user.email =~ TEMP_EMAIL_REGEX
+      redirect_to edit_user_path(@user)
+    else
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'vkontakte') if is_navigational_format?
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,8 @@
 #
 
 class User < ActiveRecord::Base
+
+  # TEMP_EMAIL_REGEX = /.temp/
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
