fa3257f6a37f8508855a415792c89b76dcdaccff
pustserg <psakaskif@gmail.com>
2015-01-20 20:43:47 +0300

    some refactor

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,6 +2,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   before_action :oauth_user
 
+  before_action :oauth_user
+
   def facebook
   end
 
@@ -31,32 +33,22 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def twitter
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user.email =~ USER::TEMP_EMAIL_REGEX
-      redirect_to edit_user_path(@user)
-    else
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'twitter') if is_navigational_format?
-    end
   end
 
   def github
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user.email =~ USER::TEMP_EMAIL_REGEX
-      redirect_to edit_user_path(@user)
-    else
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'github') if is_navigational_format?
-    end
   end
 
   def vkontakte
-        @user = User.find_for_oauth(request.env['omniauth.auth'])
+  end
+
+  private
+  def oauth_user
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user.email =~ TEMP_EMAIL_REGEX
       redirect_to edit_user_path(@user)
     else
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'vkontakte') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: 'facebook') if is_navigational_format?
     end
   end
 
