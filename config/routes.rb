0a13135dc8fb743e6e03aa4e5ced031392f1d457
pustserg <psakaskif@gmail.com>
2015-01-20 15:02:21 +0300

    temp commit before email validation

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,10 @@ Rails.application.routes.draw do
     end
   end
 
+  # resources :users do
+  #   get :profile, on: :member
+  # end
+
   get '/profile/:id', to: 'users#show', as: 'profile'
   match '/users/:id/finish_signup', to: 'users#finish_signup', as: 'finish_signup', via: [:get, :patch]
   get '/tag/:tag_name', to: 'questions#index', as: 'tag'
