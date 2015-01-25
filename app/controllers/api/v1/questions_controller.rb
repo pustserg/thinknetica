ff075efb769752ff8f1060a595840ff00f2dcea0
pustserg <psakaskif@gmail.com>
2015-01-25 22:33:55 +0300

    the second screencast for api

---
+++ app/controllers/api/v1/questions_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::QuestionsController < Api::V1::ApiController
+
+  def index
+    @questions = Question.all
+    respond_with @questions
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
       resource :profiles do
         get :me, on: :collection
       end
+      resources :questions
     end
   end
 
