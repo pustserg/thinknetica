f8f0d017693e3902ed1f0a2a25f78a2a612e46e9
pustserg <psakaskif@gmail.com>
2015-01-26 08:32:47 +0300

    profiles list

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::ProfilesController < Api::V1::ApiController
     respond_with current_resource_owner
   end
 
+  def index
+    @profiles = User.where.not(id: current_resource_owner.id)
+    respond_with @profiles
+  end
+
 end
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -37,7 +37,49 @@ describe 'Profiles API' do
           expect(response.body).to_not have_json_path(attr)
         end
       end
+    end
+  end
 
+  describe 'GET /profiles' do
+    context 'unauthorized' do
+
+      it 'returns 401 when there not access token' do
+        get '/api/v1/profiles', format: :json
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 when access token is invalid' do
+        get '/api/v1/profiles', format: :json, access_token: '12345'
+        expect(response.status).to eq 401
+      end
+    end  
+
+    context 'authorized' do
+      let!(:users) { create_list(:user, 3) }
+      let(:access_token) { create(:access_token, resource_owner_id: users.first.id) }
+      let(:profile) { users.last }
+
+      before { get '/api/v1/profiles', format: :json, access_token: access_token.token }
+
+      it 'returns 200 status' do
+        expect(response).to be_success
+      end
+
+      it 'return list of profiles without users.first' do
+        expect(response.body).to have_json_size(2).at_path('profiles')
+      end
+
+      %w(email id).each do |attr|
+        it "object contains #{attr}" do
+          expect(response.body).to be_json_eql(profile.send(attr.to_sym).to_json).at_path("profiles/1/#{attr}")
+        end
+      end
+
+      %w(password encrypted_password).each do |attr|
+        it "object does not contain #{attr}" do
+          expect(response.body).to_not have_json_path("profiles/1/#{attr}")
+        end
+      end
     end
   end
 end
