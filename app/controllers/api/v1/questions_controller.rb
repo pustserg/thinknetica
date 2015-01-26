4d80517c77354c615c286d99f0f2772257d3c0d1
pustserg <psakaskif@gmail.com>
2015-01-26 10:50:08 +0300

    questions list with comments and attachments

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::QuestionsController < Api::V1::ApiController
 
+  load_resource
+
   def index
     @questions = Question.all
     respond_with @questions
+  end
+
+  def show
+    respond_with @question
   end
 
 end
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -56,4 +56,85 @@ describe 'Questions API' do
     end
 
   end
+
+  describe 'GET /show' do
+    let!(:question) { create(:question) }
+    context 'unauthorized' do
+
+      it 'returns 401 when there not access token' do
+        get "/api/v1/questions/#{question.id}", format: :json
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 when access token is invalid' do
+        get "/api/v1/questions/#{question.id}", format: :json, access_token: '12345'
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let(:user) { create(:user) }
+      let(:access_token) { create(:access_token) }
+      let!(:answer) { create(:answer, question: question) }
+      let!(:comment) { create(:question_comment, commentable: question, user: user) }
+      let!(:attachment) { create(:attachment, attachmentable: question) }
+       
+      before { get "/api/v1/questions/#{question.id}", format: :json, access_token: access_token.token }  
+
+      it 'returns 200 status code' do
+        expect(response).to be_success
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "question object contains #{attr}" do
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("question/#{attr}")
+        end
+      end 
+
+      context 'answers' do
+        it 'included in question object' do
+          expect(response.body).to have_json_size(1).at_path('question/answers')
+        end
+
+        %w(id body created_at updated_at).each do |attr|
+          it "contains #{attr}" do
+            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("question/answers/0/#{attr}")
+          end
+        end
+      end
+
+      context 'comments' do
+        it 'included in question object' do
+          expect(response.body).to have_json_size(1).at_path('question/comments')
+        end
+
+        %w(body created_at updated_at).each do |attr|
+          it "contains #{attr}" do
+            expect(response.body).to be_json_eql(comment.send(attr.to_sym).to_json).at_path("question/comments/0/#{attr}")
+          end
+        end
+      end
+
+      context 'attachments' do
+        it 'included in question object' do
+          expect(response.body).to have_json_size(1).at_path('question/attachments')
+        end
+
+        %w(id created_at updated_at).each do |attr|
+          it "contains #{attr}" do
+            expect(response.body).to be_json_eql(attachment.send(attr.to_sym).to_json).at_path("question/attachments/0/#{attr}")
+          end
+        end
+
+        it 'file is a json object' do
+          expect(response.body).to have_json_size(1).at_path('question/attachments/0/file')
+        end
+
+        it 'contains file url' do
+          expect(response.body).to be_json_eql(attachment.file.url.to_json).at_path('question/attachments/0/file/url')
+        end
+      end
+
+    end
+  end
 end
