e96e4002044549198398145edb8d9a3819bf4a89
pustserg <psakaskif@gmail.com>
2015-02-07 15:23:59 +0300

    added checkboxes to search form

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,19 @@ class SearchController < ApplicationController
   skip_authorization_check
 
   def search
-    query = Sunspot.search(Question, Answer, Comment) do
+    if params[:questions_only]
+      search_classes = Question
+    elsif params[:answers_only]
+      search_classes = Answer
+    elsif params[:comments_only]
+      search_classes = Comment
+    else
+      search_classes = Question, Answer, Comment
+    end
+
+    query = Sunspot.search(search_classes) do
       keywords params[:search] do
-        highlight :title
+        # highlight :title
       end
     end
 
