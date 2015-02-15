fc8a76999cdff1721b5ce5d22ec6c0514ee7e161
pustserg <psakaskif@gmail.com>
2015-02-15 15:26:25 +0300

    added capistrano for deploy

---
+++ config/deploy.rb
@@ -0,0 +1,129 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+
+set :application, 'thinknetica'
+set :repo_url, 'git@github.com:pustserg/thinknetica.git'
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deployer/thinknetica'
+set :deploy_user, 'deployer'
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, %w(config/database.yml config/sunspot.yml config/private_pub.yml config/private_pub_thin.yml .env)
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, %w(bin log tmp/pids tmp/cache shared/solr tmp/sockets vendor/bundle public/system public/uploads)
+
+namespace :deploy do
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  before :updated, :setup_solr_data_dir do
+    on roles(:app) do
+      unless test "[ -d #{shared_path}/solr/data ]"
+        execute :mkdir, "-p #{shared_path}/solr/data"
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+end
+
+namespace :private_pub do
+  
+  desc 'Start private_pub server'
+  task :start do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml start"
+        end
+      end
+    end
+  end
+
+  desc 'Stop private_pub server'
+  task :stop do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml stop"
+        end
+      end
+    end
+  end
+
+  desc 'restart private_pub server'
+  task :restart do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml restart"
+        end
+      end
+    end
+  end
+
+end
+
+after 'deploy:restart', 'private_pub:restart'
+
+namespace :solr do
+  
+  %w[start stop].each do |command|
+    desc "#{command} solr"
+    task command do
+      on roles(:app) do
+        solr_pid = "#{shared_path}/pids/sunspot-solr.pid"
+        if command == "start" or (test "[ -f #{solr_pid} ]" and test "kill -0 $( cat #{solr_pid} )")
+          within current_path do
+            with rails_env: fetch(:rails_env, 'production') do
+              execute :bundle, 'exec', 'sunspot-solr', command, "--port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  desc "restart solr"
+  task :restart do
+    invoke 'solr:stop'
+    invoke 'solr:start'
+  end
+  
+  after 'deploy:finished', 'solr:restart'
+  
+  desc "reindex the whole solr database"
+  task :reindex do
+    invoke 'solr:stop'
+    on roles(:app) do
+      execute :rm, "-rf #{shared_path}/solr/data"
+    end
+    invoke 'solr:start'
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env, 'production') do
+          info "Reindexing Solr database"
+          execute :bundle, 'exec', :rake, 'sunspot:solr:reindex[,,true]'
+        end
+      end
+    end
+  end
+  
+end
